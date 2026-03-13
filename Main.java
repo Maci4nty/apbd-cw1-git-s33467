4b076b5869718bcd5240ed69c02789815d373e6b
Maciej Kowalski <s33467@pjwstk.edu.pl>
2026-03-13 20:23:41 +0100

    Dodanie try catch w razie nieprawidłowego typu danych

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        try { 
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        int sum = a * b;
-        System.out.println(sum);
+            int sum = a * b;
+       
+            System.out.println(sum);
+        } catch (InputMismatchException e) {
+            System.out.println("Nieprawidłowy typ danych");
+        }
 
         sc.close();
     }
